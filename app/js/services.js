3176bcd011cd7b31432a12146987aa5e81ad0af0
Tom <twest2jmj@yahoo.com>
2015-11-30 20:31:24 -0600

    Initial commit

---
+++ app/js/services.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* Services */
+
+var phonecatServices = angular.module('phonecatServices', ['ngResource']);
+
+phonecatServices.factory('Phone', ['$resource',
+  function($resource){
+    return $resource('phones/:phoneId.json', {}, {
+     /*
+      query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+      */
+      query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+    });
+  }]);
